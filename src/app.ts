e0ff5600ebd442eed4cb317f4c720c2b8bbb410a
Stablest <marlonlocci@hotmail.com>
2023-10-21 17:10:49 -0300

    bug-fix: express.json() is now typed correctly

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { userRouter } from "./modules/user/user.router";
+import { errorHandler } from "./middlewares/error-handler";
 
 const apiPath = "/api/v1";
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(`${apiPath}/user`, userRouter);
 
 export { app };
