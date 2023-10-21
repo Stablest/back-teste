06ca4b841953552c0a6376c48bb8874af386f6f4
Stablest <marlonlocci@hotmail.com>
2023-10-21 15:18:01 -0300

    feat: Linked loginUser and registerUser controllers with router

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { getAllUsers, loginUser, registerUser } from "./user.controller";
 
 const userRouter = express.Router();
 
-userRouter.route("/auth").get().post();
+userRouter.route("/auth").get(loginUser).post(registerUser);
 
 userRouter.route("/").get().post();
 
