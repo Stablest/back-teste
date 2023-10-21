a3d66caf0f551d4b513db4734144654855ca51f7
Stablest <marlonlocci@hotmail.com>
2023-10-21 16:52:34 -0300

    feat: Changed getUserById, updateUserById and deleteUserById routes

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -1,10 +1,26 @@
 import express from "express";
-import { getAllUsers, loginUser, registerUser } from "./user.controller";
+import {
+  deleteUserById,
+  getAllUsers,
+  getUserById,
+  loginUser,
+  registerUser,
+  updateUserById,
+} from "./user.controller";
+import { adminCheck } from "../../middlewares/admin-check";
 
 const userRouter = express.Router();
 
-userRouter.route("/auth").get(loginUser).post(registerUser);
+userRouter.route("/auth").get(loginUser);
+userRouter
+  .route("/")
+  .get(adminCheck, getAllUsers)
+  .post(adminCheck, registerUser);
 
-userRouter.route("/").get().post();
+userRouter
+  .route("/:id")
+  .get(adminCheck, getUserById)
+  .patch(adminCheck, updateUserById)
+  .delete(adminCheck, deleteUserById);
 
 export { userRouter };
