6374140eee3c3f408e8e379dda9cb9106f878390
Stablest <marlonlocci@hotmail.com>
2023-10-22 20:08:10 -0300

    feat: Added cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import { userRouter } from "./modules/user/user.router";
 import { errorHandler } from "./middlewares/error-handler";
+import cors from "cors";
 
 const apiPath = "/api/v1";
 const app = express();
 
 app.use(express.json());
-app.use(`${apiPath}/user`, userRouter);
+app.use(
+  `${apiPath}/user`,
+  cors({ credentials: true, origin: "http://localhost:3000" }),
+  userRouter
+);
 app.use(errorHandler);
 
 export { app };
